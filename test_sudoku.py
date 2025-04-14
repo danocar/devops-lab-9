81001613ed037112195291b438b9c7e25cce89f1
Daniel Carson <danielcarson@Daniels-Laptop.local>
2025-04-14 12:47:30 +0100

    first commit

---
+++ test_sudoku.py
@@ -0,0 +1,76 @@
+import unittest
+from sudoku import solve_sudoku
+
+class TestSudokuSolver(unittest.TestCase):
+
+    def test_easy_board(self):
+        board = [
+            [5, 3, 0, 0, 7, 0, 0, 0, 0],
+            [6, 0, 0, 1, 9, 5, 0, 0, 0],
+            [0, 9, 8, 0, 0, 0, 0, 6, 0],
+            [8, 0, 0, 0, 6, 0, 0, 0, 3],
+            [4, 0, 0, 8, 0, 3, 0, 0, 1],
+            [7, 0, 0, 0, 2, 0, 0, 0, 6],
+            [0, 6, 0, 0, 0, 0, 2, 8, 0],
+            [0, 0, 0, 4, 1, 9, 0, 0, 5],
+            [0, 0, 0, 0, 8, 0, 0, 7, 9]
+        ]
+        solved = solve_sudoku(board)
+        self.assertIsNotNone(solved)
+        self.assertTrue(all(all(cell != 0 for cell in row) for row in solved))
+
+    def test_already_solved(self):
+        board = [
+            [5,3,4,6,7,8,9,1,2],
+            [6,7,2,1,9,5,3,4,8],
+            [1,9,8,3,4,2,5,6,7],
+            [8,5,9,7,6,1,4,2,3],
+            [4,2,6,8,5,3,7,9,1],
+            [7,1,3,9,2,4,8,5,6],
+            [9,6,1,5,3,7,2,8,4],
+            [2,8,7,4,1,9,6,3,5],
+            [3,4,5,2,8,6,1,7,9]
+        ]
+        self.assertEqual(solve_sudoku(board), board)
+
+    def test_unsolvable_board(self):
+        board = [
+            [5, 1, 6, 8, 4, 9, 7, 3, 2],
+            [3, 0, 7, 6, 0, 5, 0, 0, 0],
+            [8, 0, 9, 7, 0, 0, 0, 6, 5],
+            [1, 3, 5, 0, 6, 0, 9, 0, 7],
+            [4, 7, 2, 5, 9, 1, 0, 0, 6],
+            [9, 6, 8, 3, 7, 0, 5, 0, 0],
+            [2, 5, 3, 1, 8, 6, 0, 7, 4],
+            [6, 8, 4, 2, 0, 7, 0, 5, 0],
+            [7, 9, 1, 0, 5, 0, 6, 0, 8]
+        ]
+        self.assertIsNone(solve_sudoku(board))
+
+    def test_blank_board(self):
+        board = [[0]*9 for _ in range(9)]
+        solved = solve_sudoku(board)
+        self.assertIsNotNone(solved)
+        self.assertTrue(all(all(cell != 0 for cell in row) for row in solved))
+
+    def test_partial_board(self):
+        board = [
+            [0, 0, 0, 2, 6, 0, 7, 0, 1],
+            [6, 8, 0, 0, 7, 0, 0, 9, 0],
+            [1, 9, 0, 0, 0, 4, 5, 0, 0],
+            [8, 2, 0, 1, 0, 0, 0, 4, 0],
+            [0, 0, 4, 6, 0, 2, 9, 0, 0],
+            [0, 5, 0, 0, 0, 3, 0, 2, 8],
+            [0, 0, 9, 3, 0, 0, 0, 7, 4],
+            [0, 4, 0, 0, 5, 0, 0, 3, 6],
+            [7, 0, 3, 0, 1, 8, 0, 0, 0]
+        ]
+        solved = solve_sudoku(board)
+        self.assertIsNotNone(solved)
+
+    def test_invalid_input_type(self):
+        with self.assertRaises(TypeError):
+            solve_sudoku("not a board")
+
+if __name__ == '__main__':
+    unittest.main()
